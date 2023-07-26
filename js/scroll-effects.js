80ff8de9535155f8c51e70a178daa74a8f587e5c
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-07-26 20:01:34 +0530

    feat: add new scroll effects, add resume section.

--- js/scroll-effects.js
+++ js/scroll-effects.js
@@ -1,7 +1,7 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(), scrollActive()};
 
 function scrollFunction() {
-
+    
     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
 
         document.getElementById("header").style.top = "0";
@@ -9,3 +9,25 @@ function scrollFunction() {
         document.getElementById("header").style.top = "-50px";
     }
 }
+
+//This part for showing active link in navbar
+
+const sections = document.querySelectorAll("section");
+const navA = document.querySelectorAll(".header-middle a");
+
+function scrollActive(){
+    
+        let fromTop = window.scrollY;
+    
+        navA.forEach((a) => {
+            const section = document.querySelector(a.getAttribute("href"));
+            if (section.offsetTop <= fromTop + 50 && section.offsetTop + section.offsetHeight > fromTop + 50) {
+                a.classList.add("active");
+            } else {
+                a.classList.remove("active");
+            }
+        });
+    
+    
+  };
+  
