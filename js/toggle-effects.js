b0e483809a7f8d19cabf3a735088f28d670793a0
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-05 16:32:11 +0530

    feat: add some responsiveness to web page

--- js/toggle-effects.js
+++ js/toggle-effects.js
@@ -1,7 +1,12 @@
 function btnTog(event){
     event.preventDefault();
-    var eduBtn = document.querySelector(".edu-topic");
+    var eduBtn = document.querySelector(".edu-desc-col2");
     console.log(eduBtn.classList);
+    eduBtn.classList.toggle(eduBtn.classList[0]);
     eduBtn.classList.toggle(eduBtn.classList[1]);
-    eduBtn.classList.toggle(eduBtn.classList[2]);
+    if(eduBtn.style.display == "none"){
+        eduBtn.style.display == "block"
+    }else if(eduBtn.style.display == "block"){
+        eduBtn.style.display == "none"
+    }
 }
