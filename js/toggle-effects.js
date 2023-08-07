25c08911df033b06892104865121f779604e8849
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-07 23:14:39 +0530

    feat: add toggle icon to education field

--- js/toggle-effects.js
+++ js/toggle-effects.js
@@ -1,8 +1,10 @@
 function btnTogEdu(event){
     event.preventDefault();
     var eduBtn = document.getElementById("edu-topic");
+    var toggleIcon = document.getElementById("toggle-icon");
     console.log(eduBtn.classList);
     eduBtn.style.display = (eduBtn.style.display === "none") ? "block" : "none";
+    toggleIcon.innerHTML = (eduBtn.style.display === "none") ? '<i class="fas fa-plus"></i>' : '<i class="fas fa-minus"></i>';
     
 }
 
