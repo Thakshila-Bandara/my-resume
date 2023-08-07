d3fbaf7f18182dfe84c53995fefb56ce0c1ae58b
Thakshila-Bandara <bandara-se19016@stu.kln.ac.lk>
2023-08-07 23:38:08 +0530

    feat: add toggle button to skill part

--- js/toggle-effects.js
+++ js/toggle-effects.js
@@ -1,8 +1,8 @@
 function btnTogEdu(event){
     event.preventDefault();
     var eduBtn = document.getElementById("edu-topic");
-    var toggleIcon = document.getElementById("toggle-icon");
-    console.log(eduBtn.classList);
+    var toggleIcon = document.getElementById("toggle-icon-edu");
+    // console.log(eduBtn.classList);
     eduBtn.style.display = (eduBtn.style.display === "none") ? "block" : "none";
     toggleIcon.innerHTML = (eduBtn.style.display === "none") ? '<i class="fas fa-plus"></i>' : '<i class="fas fa-minus"></i>';
     
@@ -11,7 +11,9 @@ function btnTogEdu(event){
 function btnTogSkill(event){
     event.preventDefault();
     var skillBtn = document.getElementById("skills-topic");
-    console.log(skillBtn.classList);
+    var toggleIconSkill = document.getElementById("toggle-icon-skill");
+    // console.log(skillBtn.classList);
     skillBtn.style.display = (skillBtn.style.display === "none") ? "block" : "none";
+    toggleIconSkill.innerHTML = (skillBtn.style.display === "none") ? '<i class="fas fa-plus"></i>' : '<i class="fas fa-minus"></i>';
     
 }
